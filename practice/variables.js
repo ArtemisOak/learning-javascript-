eecf71030eb24cca9cc2cf3a67dc2fcb8f75a9f6
Artemis <thompsonaj4@s.dcsdk12.org>
2022-11-02 16:19:42 -0600

    added arrays and objects

---
+++ practice/variables.js
@@ -0,0 +1,19 @@
+let x = 500;
+let y = 250;
+
+let myName = "Artemis";
+myName.length;
+
+let ages = [25, 17, 16, 85];
+console.log("ages is an array", ages);
+
+let student = {
+	name: ":)",
+	grade: "A-",
+	gpa: 3.666,
+	isCool: true,
+	friends: ["Bee", "Bunny", "KK"]
+};
+console.log("student is", student);
+console.log("student name is", student.name);
+student.gradeLevel = 12;
